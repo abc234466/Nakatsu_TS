888fb2ffcce24fed2def63ed7d4f8cf9bf9d75ec
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2017-09-11 15:25:13 +0800

    Update DS_Nakatsu
    Remove nonsence

--- DS_Nakatsu.py
+++ DS_Nakatsu.py
@@ -1,51 +1,25 @@
+'''
+    i must be larger than s -> i >= s
 
 
 
-# dvalue = [[0 for i in range(len(str2))] for i in range(len(str1))]
+'''
 
-
-# def dsNakatsu():
-#     i = 0
-#     j = 0
-#     ci = i
-#     cj = j
-#     while ci < len(str1)  or cj < len(str2) :
-#         while str1[ci] != str2[cj] and cj < len(str2) - 1:
-#             cj += 1
-#         if cj != len(str2) - 1 and cj > dvalue[ci - 1][cj - 1]:
-#             dvalue[ci][cj] = min(cj, dvalue[ci - 1][cj])
-#             ci += 1
-#             cj += 1
-#         else:
-#             dvalue[ci][cj] = dvalue[ci - 1][cj]
-#             i+=1
-#             ci = i
-#             cj = j
-#
-#
-#
-# dsNakatsu()
-
-
-# print(dvalue)
-
-str1 = "actgcttcg"
+str1 = "actcg"
 str2 = "tcacgtcc"
 
 
 def d(i, s):
-    if i < s:
-        return float('inf')
-    elif s == 0:
+    if s == 0:
         return 0
-    j = d(i - 1, s - 1)
-    if j == float('inf'):
-        return ('inf')
+    elif i < s or d(i - 1, s - 1) == float('inf'):
+        return float('inf')
+    j = d(i - 1, s - 1)  # lower bound
     while str1[i - 1] != str2[j] and j < len(str2):
         j += 1
         if j == len(str2):
             break
-    if j < len(str2) :
+    if j < len(str2):
         return min(j + 1, d(i - 1, s))
     else:
         return d(i - 1, s)
