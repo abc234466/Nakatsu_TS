cc5fb5d2a745c4e105bde68f997cda383f6b53ea
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2017-09-12 17:23:56 +0800

    Rename DS_Nakatsu to DP_Nakatsu
    Update DP_Nakatsu

---
+++ DP_Nakatsu.py
@@ -0,0 +1,33 @@
+'''
+    Nakatsu DP
+    DP -> find "lower bound"
+    i must be larger than s -> i >= s
+    s = 0 -> return 0
+
+'''
+
+str1 = "bcdabab"
+str2 = "cbacbaaba"
+
+
+def d(i, s):
+    if s == 0:
+        return 0
+    elif i < s or d(i - 1, s - 1) >= len(str2):
+        return float('inf')
+    j = d(i - 1, s - 1)  # lower bound
+    while str1[i - 1] != str2[j] and j < len(str2):  # find next match index
+        j += 1
+        if j == len(str2):
+           break
+    if j < len(str2):
+        return min(j + 1, d(i - 1, s))
+    else:
+        return d(i - 1, s)
+
+
+# for i in range(len(str1)):
+#     for j in range(len(str2)):
+#         print("{}".format(d(i, j)), end=' ')
+#     print()
+print(d(5, 4))
