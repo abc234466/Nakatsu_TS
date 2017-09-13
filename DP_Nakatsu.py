ac92d242fa2d71d204cbff6aa3308258c7805f0f
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2017-09-13 13:54:59 +0800

    Update DP_Nakatsu

--- DP_Nakatsu.py
+++ DP_Nakatsu.py
@@ -1,6 +1,6 @@
 '''
-    i must be larger than s -> i >= s
-
+    i must be larger than s , i.e. i >= s
+    i < s  is impossible
 
 
 '''
@@ -12,10 +12,11 @@ str2 = "cbacbaaba"
 def d(i, s):
     if s == 0:
         return 0
-    elif i < s or d(i - 1, s - 1) == float('inf'):
+    elif i < s or d(i - 1, s - 1) >=len(str2) :
         return float('inf')
     j = d(i - 1, s - 1)  # lower bound
-    while str1[i - 1] != str2[j] and j < len(str2):
+
+    while str1[i - 1] != str2[j] and j < len(str2):  # find next match, i.e. str1[i] == str2[j]
         j += 1
         if j == len(str2):
             break
@@ -25,4 +26,8 @@ def d(i, s):
         return d(i - 1, s)
 
 
-print(d(1, 1))
+
+for i in range(len(str1)+1):
+    for j in range(len(str2)):
+        print(d(i, j), end=' ')
+    print()
